3066d96c3baa7a20794d0a648f7df2e1bf9a9812
Yara Grassi Gouffon (RIT Student) <vaati2005-git@yahoo.com.br>
2016-04-15 14:52:29 -0400

    Got the brush working with tweening, but the code is rushed and really crappy right now. Needs fixing.

--- js/world.js
+++ js/world.js
@@ -4,6 +4,7 @@ var world = function(game){
     var colors;
     var bmcanvas;
     var bmworld;
+    var lastPosition;
 };
 
 world.prototype = {
@@ -17,13 +18,15 @@ world.prototype = {
         //TODO remove this when there is a background image
         this.game.stage.backgroundColor = "#FFFFFF";
 
+        lastPosition = null;
+
         colors = Phaser.Color.HSVColorWheel();
         i = 0;
 
         loop = this.game.make.sprite(0, 0, 'loop');
         loop.scale.set(0.5);
         loop.anchor.set(0.5);
-        //	This is the BitmapData we're going to be drawing to
+        //  This is the BitmapData we're going to be drawing to
         bmcanvas = this.game.add.bitmapData(this.game.width, this.game.height);
         bmcanvas.addToWorld();
 
@@ -34,13 +37,27 @@ world.prototype = {
 
     paint: function (pointer, x, y) {
         if (pointer.isDown) {
+            lastPosition = lastPosition || new Phaser.Point(this.game.input.activePointer.x, this.game.input.activePointer.y);
 
             //bmcanvas.circle(x, y, 4, colors[i].rgba);
             //console.log(colors[i]);
             loop.tint = (colors[i].r << 16) | (colors[i].g << 8) | colors[i].b;
             i = this.game.math.wrapValue(i, 1, 359);
 
-            bmcanvas.draw(loop, x, y, null, null, '');
+            var position = new Phaser.Point(this.game.input.activePointer.x, this.game.input.activePointer.y);
+            
+
+            var tween = this.game.make.tween(lastPosition).to(position);
+            var path = tween.generateData(60);
+
+            for(var j=0; j<path.length; j++) {
+                bmcanvas.draw(loop, path[j].x, path[j].y, null, null, '');
+
+            }
+            lastPosition = position;
+
+
+            // bmcanvas.path(loop, x, y, null, null, '');
 
             //loop.scale.set(loop.scale.x - 0.05*(this.game.time.elapsed/1000));
         }
