b668a6ca7838c53db4a7f03ee70018d26daf511f
Zachary Cupec <zcupec@gmail.com>
2016-04-18 14:51:29 -0400

    Ink size tied to ink amount and started refill code

--- js/world.js
+++ js/world.js
@@ -5,6 +5,10 @@ var world = function(game){
     var bmcanvas;
     var bmworld;
     var lastPosition;
+    var inkAmount;
+    var inkDecrease;
+    
+    var refillDot;
 };
 
 world.prototype = {
@@ -24,19 +28,23 @@ world.prototype = {
         i = 0;
 
         loop = this.game.make.sprite(0, 0, 'loop');
-        loop.scale.set(0.5);
+        loop.scale.set(0.25);
         loop.anchor.set(0.5);
+        inkAmount = loop.scale.x;
+        inkDecrease = inkAmount / 80;
         //  This is the BitmapData we're going to be drawing to
         bmcanvas = this.game.add.bitmapData(this.game.width, this.game.height);
         bmcanvas.addToWorld();
 
         bmcanvas.smoothed = false;
+        
+        refillDot = new refill(250, 250);
 
         this.game.input.addMoveCallback(this.paint, this);
     },
 
     paint: function (pointer, x, y) {
-        if (pointer.isDown) {
+        if (pointer.isDown && inkAmount > 0) {
             lastPosition = lastPosition || new Phaser.Point(this.game.input.activePointer.x, this.game.input.activePointer.y);
 
             //bmcanvas.circle(x, y, 4, colors[i].rgba);
@@ -46,7 +54,6 @@ world.prototype = {
 
             var position = new Phaser.Point(this.game.input.activePointer.x, this.game.input.activePointer.y);
             
-
             var tween = this.game.make.tween(lastPosition).to(position);
             var path = tween.generateData(60);
 
@@ -55,19 +62,33 @@ world.prototype = {
 
             }
             lastPosition = position;
-
-
-            // bmcanvas.path(loop, x, y, null, null, '');
+            
+            inkAmount -= inkDecrease;
+            console.log("inkAmount: " + inkAmount);
+            
+            //bmcanvas.path(loop, x, y, null, null, '');
 
             //loop.scale.set(loop.scale.x - 0.05*(this.game.time.elapsed/1000));
+            loop.scale.set(inkAmount);
+            //console.log("loop.scale: " + loop.scale);
         }
     },
 
     update: function() {
 
-        
+        if (lastPosition != null && Phaser.Point.distance(lastPosition, refillDot, true) < 20) {
+            inkAmount += refillDot.refillAmount;
+            console.log("Refilled!");    
+        }
 
     }
 
 };
 
+var refill = function(x, y){
+    this.refillAmount = 20;
+    this.x = x;
+    this.y = y;
+};
+refill.prototype = Object.create(Phaser.Point.prototype);
+refill.prototype.constructor = refill;
